ed2514cbeff7acab7d49717252b10565aed7d49c
Robert Davis <NRobertDavis@gmail.com>
2021-09-24 17:55:01 -0400

    Complete Project

---
+++ src/lottery/Lottery.java
@@ -0,0 +1,102 @@
+package lottery;
+
+import java.util.Random;
+
+public class Lottery {
+
+    //Private attribute for the ticket
+    private String ticket;
+
+    //Empty Lottery Constructor
+    public Lottery() {
+        ticket = "";
+    }
+
+    //Constructor that takes reference to a random object
+    public Lottery(Random randRef) {
+
+        //Generate random numbers for the tickets
+        ticket = "";
+        for (int i = 0; i < 6; i++)
+            ticket = ticket.concat(String.valueOf(randRef.nextInt(10)));
+    }
+
+    //Getter for the ticket
+    public String GetTicket() {
+        return ticket;
+    }
+
+
+    //Generate a string of random digits for the ticket
+    static String GenerateRandomWinner(Random randRef) {
+        String digits = "";
+        for (int i = 0; i < 6; i++) {
+            digits = digits.concat(String.valueOf(randRef.nextInt(10)));
+        }
+        return digits;
+    }
+
+    static int partition(Lottery[] arr, int low, int high) {
+        Lottery pivot = arr[high];
+
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (Integer.parseInt(arr[j].GetTicket()) < Integer.parseInt(pivot.GetTicket())) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+
+    static void swap(Lottery[] arr, int i, int j) {
+        Lottery temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void quickSort(Lottery[] arr, int low, int high) {
+        if (low < high) {
+            int index = partition(arr, low, high);
+
+            quickSort(arr, low, index - 1);
+            quickSort(arr, index + 1, high);
+        }
+    }
+
+    //Sort the array
+    public static void Sort(Lottery[] tickets) {
+        quickSort(tickets, 0, tickets.length - 1);
+    }
+
+    //Generate index of winner
+    public static int GenerateSelectWinner(int maxIndex, Random randRef) {
+        return randRef.nextInt(maxIndex);
+    }
+
+    public static Boolean Solution1(Lottery[] array, String sol, int maxIndex) {
+        for (int i = 0; i < maxIndex; i++)
+            if (array[i].GetTicket().equals(sol))
+                return true;
+        return false;
+    }
+
+    public static Boolean Solution2(Lottery[] array, int low, int high, String sol){
+
+        if(high >= low){
+            int mid = (low + (high -1)) / 2;
+
+            if((array[mid].GetTicket()).equals(sol))
+                return true;
+
+            if(Integer.parseInt(array[mid].GetTicket()) > Integer.parseInt(sol))
+                return Solution2(array, low, mid - 1, sol);
+
+            if(Integer.parseInt(array[mid].GetTicket()) < Integer.parseInt(sol))
+                return Solution2(array, mid + 1, high, sol);
+        }
+        return false;
+    }
+}
